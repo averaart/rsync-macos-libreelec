764870629f127aefde67a5c666a5805376ca37cd
Andra Veraart <averaart@hotmail.com>
2017-04-27 20:42:17 +0200

    Use full paths for all file references

--- LaunchDaemons/sync_to_libreelec.sh
+++ LaunchDaemons/sync_to_libreelec.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # Accepts two arguments: source directory and destination including user@host.
 destination=$(printf %q $2)
-sshpass -f libreelec-password.txt rsync -aP --delete --exclude-from=rsync-excludes.txt --rsync-path=/storage/.kodi/addons/virtual.network-tools/bin/rsync $1 $destination
+/usr/local/bin/sshpass -f "/Library/Application Support/nl.andra.rsync-to-kodi/LaunchDaemons/libreelec-password.txt" \
+  /usr/bin/rsync -aP --delete \
+                 --exclude-from="/Library/Application Support/nl.andra.rsync-to-kodi/LaunchDaemons/rsync-excludes.txt" \
+                 --rsync-path=/storage/.kodi/addons/virtual.network-tools/bin/rsync $1 $destination
