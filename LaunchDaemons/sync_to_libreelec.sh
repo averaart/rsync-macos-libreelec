5805e878f0fa7bffd041fa69d4873062b88c38b7
Andra Veraart <averaart@hotmail.com>
2017-04-27 22:11:16 +0200

    Improve handling of spaces in paths

--- LaunchDaemons/sync_to_libreelec.sh
+++ LaunchDaemons/sync_to_libreelec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Accepts two arguments: source directory and destination including user@host.
-destination=$(printf %q $2)
+destination=$(printf %q "$2")
 /usr/local/bin/sshpass -f "/Library/Application Support/nl.andra.rsync-to-kodi/LaunchDaemons/libreelec-password.txt" \
   /usr/bin/rsync -aP --delete \
                  --exclude-from="/Library/Application Support/nl.andra.rsync-to-kodi/LaunchDaemons/rsync-excludes.txt" \
-                 --rsync-path=/storage/.kodi/addons/virtual.network-tools/bin/rsync $1 $destination
+                 --rsync-path=/storage/.kodi/addons/virtual.network-tools/bin/rsync "$1" "$destination"
